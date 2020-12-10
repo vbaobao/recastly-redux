703d1b80d8f979b4813e40856415a3c577bb4eeb
jsmithb117 <jsmithb117@gmail.com>
2020-12-09 19:55:22 -0800

    Passes store tests

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,56 @@
+// import { connect } from 'react-redux';
+// import Search from './../components/Search.js';
+// import handleVideoSearch from '../actions/search.js';
+
+// var SearchContainer = (state) => {
+//   function mapStateToProps(state) {
+//     return { videoList: state.videoList};
+//   }
+
+//   function mapDispatchToProps(dispatch) {
+//     return { action: bindActionCreators(handleVideoSearch, dispatch)};
+//   }
+
+//   return connect(mapStateToProps, mapDispatchToProps)(Search);
+// };
+
+// //TODO: define a SearchContainer component which will hook up your action
+// // dispatchers with your search component props.
+// //HINT: use react-redux 'connect' method to generate a container component from
+// //state and dispatch mappings.
+
+// export default SearchContainer;
+
+
+
+//App => Search
+
+//App => SearchContainer (connect) => Search
+
+//App.js
+// SearchContainer(Search)
+//connect()(Search)
+
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
-import handleSearchChange from '../actions/search.js';
+import handleVideoSearch from '../actions/search.js';
+import { bindActionCreators } from 'redux'
 
-var SearchContainer = () => {};
+var SearchContainer = (props) => {
+
+  return <Search handleSearchInputChange={props.handleSearchInputChange}/>
+};
+function mapStateToProps(state) {
+  return { videoList: state.videoList};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { action: bindActionCreators(handleVideoSearch, dispatch)};
+}
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
 
-export default SearchContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(SearchContainer);
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,30 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-//{
-    // currentVideo: null,
-    // videos: []
-//}
+var initialState = {};
+
+const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+);
+
+export default store;
+
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './../reducers/main.js';
+// import exampleVideoData from '../data/exampleVideoData.js';
+
+// // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// var initialState = {};
+// const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+// export default store;
